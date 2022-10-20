b372d9a9f546ebb32c1441cfd97c5a6856b1a190
Amy Kapernick <amy@kapers.dev>
2022-10-20 12:11:02 +0800

    added basic site structure and layout

--- node_modules/astro/dist/vite-plugin-utils/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Data } from 'vfile';
-import type { AstroConfig, MarkdownAstroData } from '../@types/astro';
-export declare function getFileInfo(id: string, config: AstroConfig): {
-    fileId: string;
-    fileUrl: string | undefined;
-};
-export declare function safelyGetAstroData(vfileData: Data): MarkdownAstroData;
