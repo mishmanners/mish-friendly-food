25eab71cf3580d0a2a38261cb32465d260fa9fed
Michelle "MishManners®™" Duke <36594527+mishmanners@users.noreply.github.com>
2024-10-25 14:51:37 +1100

    Tests now pass
    Correct file

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -4,4 +4,79 @@ test("page opens", async ({ page }) => {
   await page.goto("/");
   await expect(page).toHaveTitle("Mish Friendly Food");
   await expect(page.locator("h1")).toHaveText("Mish Friendly Food");
+  await expect(page.locator("h2")).toHaveText("Search and Translate");
+});
+
+
+test("has a search form", async ({ page }) => {
+  await page.goto("/");
+  await expect(page.locator("form")).toBeVisible();
+});
+
+test("can translate a word", async ({ page }) => {
+  await page.goto("/");
+
+  await page.getByLabel("From").selectOption("English");
+  await page.getByLabel("To").selectOption("German");
+  await page.getByLabel("Translate food/s").fill("milk");
+
+  await page.getByRole("button", { name: "Translate" }).click();
+
+  await expect(
+    page.getByTestId("translation-results").locator("li li")
+  ).toHaveText([/Milch/, /Milchprodukt/]);
+});
+
+test("shows an error when there is no translation", async ({ page }) => {
+  await page.goto("/");
+
+  await page.getByLabel("From").selectOption("English");
+  await page.getByLabel("To").selectOption("Latin");
+  await page.getByLabel("Translate food/s").fill("milk");
+
+  await page.getByRole("button", { name: "Translate" }).click();
+
+  await expect(page.getByTestId("translation-results")).toHaveText(
+    /Translations not found for selected language combination/
+  );
+});
+
+test("translates food dietaries", async ({ page }) => {
+  await page.goto("/");
+
+  await page.getByLabel("From").selectOption("English");
+  await page.getByLabel("To").selectOption("French");
+  await page.getByLabel("Translate food/s").fill("gluten-free, vegan, dairy-free");
+
+  await page.getByRole("button", { name: "Translate" }).click();
+
+  await expect(
+    page.getByTestId("translation-results").locator("li li")
+  ).toHaveText([
+    /sans gluten/i,
+    /végétalienne/i,
+    /végétalien/i,
+    /végan/i,
+    /sans lactose/i,
+    /sans produits laitiers/i,
+  ]);
+});
+
+test("deduplicates words in search field", async ({ page }) => {
+  await page.goto("/");
+
+  await page.getByLabel("From").selectOption("English");
+  await page.getByLabel("To").selectOption("German");
+  await page.getByLabel("Translate food/s").fill("egg,egg");
+
+  await page.getByRole("button", { name: "Translate" }).click();
+
+  await expect(
+    page.getByTestId("translation-results").locator("li li")
+  ).toHaveText([
+    /Ei/,
+    /Eier/,
+    /Eiweiß/,
+    /Eigelb/,
+  ]);
 });
